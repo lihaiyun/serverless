6365e20d68ddb9e47272b4ba386bf39620ab13d3
Li Haiyun <li_haiyun@nyp.edu.sg>
2024-08-20 16:02:00 +0800

    Use environment variables

--- my-python-http-api/handler.py
+++ my-python-http-api/handler.py
@@ -1,9 +1,11 @@
 import json
-
+import os
 
 def hello(event, context):
+    my_env_var = os.getenv('MY_ENV_VAR')
     body = {
         "message": "Go Serverless v4.0! Your function executed successfully!",
+        "my_env_var": my_env_var
     }
 
     response = {"statusCode": 200, "body": json.dumps(body)}
